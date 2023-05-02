51f74e74cd6b5a9e9e98629eba5106666de58f3f
anciyaregi <anciyaregi25@gmail.com>
2023-05-02 11:56:11 +0530

    data types

---
+++ First.java
@@ -0,0 +1,47 @@
+public class First {
+    public static void main(String[] args) {
+
+        byte myn = 100;
+        System.out.println(myn);
+
+        short myNum = 5000;
+        System.out.println(myNum);
+
+        int mynumb = 100000;
+        System.out.println(mynumb);
+
+        long numb = 15000000000L;
+        System.out.println(numb);
+
+        float nums = 5.75f;
+        System.out.println(nums);
+
+        double doub = 19.99d;
+        System.out.println(doub);
+
+        boolean isJavaFun = false;
+        boolean isFishTasty = true;
+        System.out.println(isJavaFun);     // Outputs false
+        System.out.println(isFishTasty);   // Outputs true
+
+        char myGrade = 'J';
+        System.out.println(myGrade);
+
+        char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+        System.out.println(myVar1);
+        System.out.println(myVar2);
+        System.out.println(myVar3);   // Outputs ASCII
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+

d46cc90fe8cc415583e164f4abdbb58aae1af63e
anciyaregi <anciyaregi25@gmail.com>
2023-05-23 20:31:30 +0530

    method

---
+++ method.java
@@ -0,0 +1,10 @@
+public class Main {
+    static void myMethod() {
+        System.out.println("I like flowers!");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+        myMethod();
+    }
+}

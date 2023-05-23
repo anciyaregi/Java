de9413c543f7969529f3d50fad3c34846a235c8d
anciyaregi <anciyaregi25@gmail.com>
2023-05-23 21:37:29 +0530

    elseif

---
+++ elseif.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int time = 24;
+        if (time < 10) {
+            System.out.println("No!");
+        } else if (time < 18) {
+            System.out.println("No");
+        }  else {
+            System.out.println("Yes");
+        }
+    }
+}

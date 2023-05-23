08c02d7fb2e3d48e292a40e9a813a5fa86bacd5c
anciyaregi <anciyaregi25@gmail.com>
2023-05-23 21:55:46 +0530

    foreach

---
+++ foreach.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] Fruits = {"Mango", "Apple", "Orange", "Banana"};
+        for (String i : Fruits) {
+            System.out.println(i);
+        }
+    }
+}
